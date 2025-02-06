8d0cb5258a5563fd461bcb00961a6e6aaa8c3faf
agigibairene <i.agigiba@alustudent.com>
2025-02-06 15:15:23 +0200

    fetching data

--- src/Utils/constants.jsx
+++ src/Utils/constants.jsx
@@ -2,6 +2,6 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWY3ODdiNzc2MWZhN2VmMjg4MDY1MTBlNjU0M2NkMyIsIm5iZiI6MTczODg0NTQ5Mi4xMTYsInN1YiI6IjY3YTRhZDM0NzdiOGNlZDQ1NjY2ZTU5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QgATq1ZajYnf5IiNpynKYQ8ONUD8_5Fehv3e_MXclpM'
+      Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
     }
 };
