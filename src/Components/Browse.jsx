ba4211784ebe3f888dc806b7ecc96cd2418ea908
agigibairene <i.agigiba@alustudent.com>
2025-02-06 15:04:47 +0200

    fetching data

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../Utils/constants";
 
 export default function Browse(){
+    const [movies, setMovies] = useState();
+
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
+    const fetchMovies = async () =>{
+        const response = await fetch(url, API_OPTIONS);
+        const data = await response.json();
+        setMovies(data.results)
+    }
+
+    useEffect(()=>{
+        fetchMovies();
+    }, []);
+
+    console.log(movies);
+
     return(
         <section>
             <Header />
