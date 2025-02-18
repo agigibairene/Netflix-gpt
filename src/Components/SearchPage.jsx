b3621712db00dafae382c2ab153b4ea7f9f9b76a
agigibairene <i.agigiba@alustudent.com>
2025-02-18 23:59:34 +0200

    mistral ai

--- src/Components/SearchPage.jsx
+++ src/Components/SearchPage.jsx
@@ -1,47 +1,34 @@
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 import GptMovieSuggestions from "./GptMovieSuggestions";
 import lang from "../Utils/languageConstants";
 import { useSelector } from "react-redux";
-import OpenAI from "openai";
+import { Mistral } from '@mistralai/mistralai';
 
 
-const client = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
-  });
-  
-  async function main(userQueryTxt) {
-    try{
-        const chatCompletion = await client.chat.completions.create({
-            messages: [{ role: "user", content: userQueryTxt }],
-            model: "gpt-3.5-turbo",
-        });
-        console.log(chatCompletion.choices);
-    }
-    catch(e){
-        console.log(e);
-    }
-  }
+
 
 export default function SearchPage(){
-    const [searchText, setSearchText] = useState("");
+    const searchText = useRef();
     const chosenLang = useSelector(state=>state.config.lang);
-    const [debouncedSearchText, setDebouncedSearchText] = useState("");
 
-    useEffect(() => {
-        const timeout = setTimeout(() => setDebouncedSearchText(searchText), 500);
-        return () => clearTimeout(timeout);
-      }, [searchText]
-    );
 
-     
-   function handleGptSearch(){
-        const gptQuery =`Act as a Movie Recommendation system and suggest some movies for the query
-        ${debouncedSearchText}. Only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya`;
-        if (debouncedSearchText.trim()){
-            main(gptQuery);
-        }
+    async function handleGpt(){
+        const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+        const client = new Mistral({apiKey: apiKey});
+        const gptQuery =
+        "Act as a Movie Recommendation system and suggest some movies for the query: " +
+        searchText.current.value +
+        ". Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+
+        const chatResponse = await client.chat.complete({
+        model: 'mistral-large-latest',
+        messages: [{role: 'user', content: gptQuery}],
+        });
+
+        console.log('Chat:', chatResponse.choices[0].message.content);
     }
+    
 
 
     return(
@@ -51,10 +38,9 @@ export default function SearchPage(){
                     type="GPT search"
                     className="w-[350px] bg-white outline-none border-0 px-6 py-3 rounded-lg"
                     placeholder={lang[chosenLang]?.placeholder}
-                    value={searchText}
-                    onChange={(e)=>setSearchText(e.target.value)}
+                    ref={searchText}
                 />
-                <button onClick={handleGptSearch} className="bg-netflix-color text-white px-4 py-3 font-semibold rounded outline-none">{lang[chosenLang]?.search}</button>
+                <button onClick={handleGpt} className="bg-netflix-color text-white px-4 py-3 font-semibold rounded outline-none">{lang[chosenLang]?.search}</button>
             </form>
 
             <GptMovieSuggestions />
